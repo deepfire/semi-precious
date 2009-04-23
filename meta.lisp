484ecf6cc3951b4a8bb2858f148e03b6be4f0810
Samium Gromoff <_deepfire@feelingofgreen.ru>
2009-04-23 17:01:46 +0400

    FUNCTION-ARGLIST argh.

--- meta.lisp
+++ meta.lisp
@@ -54,8 +54,7 @@
   (:method ((type (eql 'class))) "es"))
 
 (defun function-arglist (fn)
-  (let ((arglist #+sbcl (sb-introspect:function-lambda-list fn)
-                 #-sbcl nil))
+  (let ((arglist #+(or) (sb-introspect:function-lambda-list fn)))
     (subseq arglist 0 (position '&aux arglist))))
 
 (defgeneric arglist (x type)
